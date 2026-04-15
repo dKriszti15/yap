131ae34447d0eb5ad8b55582929c112032bc036b
daczk <dacz.krisztian@stud.ubbcluj.ro>
2026-04-15 15:32:11 +0300

    Created profilePage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { Route, Router } from "@solidjs/router";
 import AuthPage from "./pages/authPage.jsx";
 import HomePage from "./pages/homePage.jsx";
 import NavBar from "./components/navBar.jsx";
+import ProfilePage from "./pages/profilePage.jsx";
 
 function AppLayout(props: { children?: unknown }) {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Route path="/" component={AppLayout}>
         <Route path="" component={HomePage} />
         <Route path="auth" component={AuthPage} />
+        <Route path="profile" component={ProfilePage} />
       </Route>
     </Router>
   );
---
+++ frontend/src/pages/profilePage.tsx
@@ -0,0 +1,14 @@
+import { A } from "@solidjs/router";
+
+export default function ProfilePage(){
+    return(
+        <>
+            <main class="home-page">
+                <section class="home-card">
+                    <p class="home-kicker">Yap</p>
+                    <h2>PROFILE PAGE</h2>
+                </section>
+            </main>
+        </>
+    )
+}
